f5c7e063d6f3697b2152b6c7dbc146da0d0c1d4a
friscolie <68372390+friscolie@users.noreply.github.com>
2020-09-02 11:46:26 +0700

    limit 10 list

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -7,22 +7,30 @@ import Todos from "../components/Todos";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
-    { text: "Learning React!" },
-    { text: "Learning React Hooks!" },
-    { text: "Learning styling in React!" }
+    { text: "Learning React!", isCompleted: false },
+    { text: "Learning React Hooks!", isCompleted: false },
+    { text: "Learning styling in React!", isCompleted: false }
   ]);
 
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value }];
+    if (todos.length < 10) {
+      const addedTodo = [...todos, { text: value, isCompleted: false }];
+      setTodos(addedTodo);
+    } else {
+      alert("The list has reached maximum space");
+    }
+  };
+
+  const completeTodo = (index) => {
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = !addedTodo[index].isCompleted;
 
     setTodos(addedTodo);
   };
 
   const showAddToggle = () => setShowAdd(!showAdd);
-
-  console.log(showAdd);
 
   return (
     // paper disini menggantikan div class container dan frame
@@ -30,7 +38,7 @@ const TodoList = () => {
     <Paper>
       <Header showAddToggle={showAddToggle} showAdd={showAdd} />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} />
+      <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
   );
 };
