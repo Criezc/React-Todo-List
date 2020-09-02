b955ef81119cddaf4f28ba2bacce5965708021ea
friscolie <68372390+friscolie@users.noreply.github.com>
2020-09-02 13:10:58 +0700

    delete function and placeholder

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -30,13 +30,18 @@ const TodoList = () => {
     setTodos(addedTodo);
   };
 
+  const clearTodos = () => setTodos([]);
   const showAddToggle = () => setShowAdd(!showAdd);
 
   return (
     // paper disini menggantikan div class container dan frame
     // yang di link dari Paper.jsx
     <Paper>
-      <Header showAddToggle={showAddToggle} showAdd={showAdd} />
+      <Header
+        showAddToggle={showAddToggle}
+        showAdd={showAdd}
+        clearTodos={clearTodos}
+      />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
       <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
